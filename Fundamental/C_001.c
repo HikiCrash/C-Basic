d939f267f8f280530187a2905f9d4b3ab862d1eb
HikiCrash <csh02127@naver.com>
2025-10-28 12:29:00 +0900

    C_002.c add

--- Fundamental/C_001.c
+++ Fundamental/C_001.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
-int main(){
-    printf("welcome to C world\n");
+int main(){                      // void main은 더이상 사용하지 않는 형식이다.
+    printf("welcome to C world\n"); 
+    
+    return 0;
 }
---
+++ Fundamental/C_002.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int sum;
+    sum = 10 + 20;
+    printf("Sum of 10 and 20 is %d \n", sum);
+
+    return 0;
+}
