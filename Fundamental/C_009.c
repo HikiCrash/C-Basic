6e5b251bb03ce250e131318aa888dbeac8ffa15c
HikiCrash <csh02127@naver.com>
2025-10-28 15:14:46 +0900

    C_010.c add

--- Fundamental/C_009.c
+++ Fundamental/C_009.c
@@ -6,7 +6,7 @@ int main(){
     printf("%s \n", str);
     printf("%17s \n", str); // 17자리 오른쪽 정렬
     printf("%-17s \n", str); // 17자리 왼쪽 정렬
-    printf("%017s \n", str); // 17자리 오른쪽 정렬, 공백은 0
+    printf("%017s \n", str); // 17자리 오른쪽 정렬, 문자열은 공백 채우기X %17s와 같음.
     printf("%11.5s \n", str); // 11자리, 5글자 출력, 오른쪽 정렬
     printf("%-11.5s \n", str); // 11자리, 5글자 출력, 왼쪽 정렬
 
