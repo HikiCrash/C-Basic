d1137b05da97e8d833b6d582b2c37e88289aab2f
HikiCrash <csh02127@naver.com>
2025-12-01 02:44:20 +0900

    C_043.c add

---
+++ Fundamental/C_043.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int code = 0;
+
+    printf("코드(1~3) 입력 (9를 누르면 종료): ");
+    
+    while (code != 9)
+    {
+        putchar('\n');
+        scanf("%d", &code);
+        switch(code)
+        {
+            case 1: printf("입력한 코드는 1 입니다.");
+                break;
+            case 2: printf("입력한 코드는 2 입니다.");
+                break;
+            case 3: printf("입력한 코드는 3 입니다.");
+                break;
+            case 9: printf("프로그램을 종료합니다.");
+                break;
+            default: printf("잘못 입력 하였습니다.");
+        }
+    }
+    
+    return 0;
+}
