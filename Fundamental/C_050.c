f1551ce80957242617b245f1670e9336234adee7
HikiCrash <csh02127@naver.com>
2025-12-11 23:49:10 +0900

    C_050.c add

---
+++ Fundamental/C_050.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    int i = 1;
+
+    printf("구구단 단 : ");
+    scanf("%d", &n);
+    
+    while (i <= 9)
+    {
+        printf("%d * %d = %d \n", n, i, n*i );
+        i++;
+    }
+    
+    return 0;
+}
