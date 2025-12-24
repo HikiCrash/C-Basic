287bb8a7b7de5c45e3df581817d9a1a67c824543
HikiCrash <csh02127@naver.com>
2025-12-24 23:35:28 +0900

    C_061.c add

---
+++ Fundamental/C_061.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int spc(int n);
+
+int main(){
+    int i;
+
+    for ( i = 0; i < 5; i++)
+    {
+        spc(i); // i개의 공백 출력.
+        printf("C Language \n");
+    }
+
+    return 0;
+}
+
+int spc(int n){
+    int i;
+
+    for (i = 0; i < n; i++) // n개의 공백 출력.
+        printf(" ");
+
+    return 0;
+} // i는 각 함수에서만 쓰는 지역 변수다.  
