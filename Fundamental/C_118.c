e35a56915655d46f2368431f31f11ea4b2ce5dff
HikiCrash <csh02127@naver.com>
2026-03-10 06:48:02 +0900

    C_118.c add

---
+++ Fundamental/C_118.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define MAX 1000 // 매크로 상수 MAX를 1000으로 정의
+
+int main(){
+    #if MAX > 999 // 전처리기 조건문 MAX가 999보다 크면 아래 컴파일.
+        printf("compiled folr array greater than 999\n"); // 참일 때 실행.
+    #endif // 조건문 종료.
+    
+    return 0;
+}
